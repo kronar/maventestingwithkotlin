029e9c8e2e852be2a2e2ee0815228b13c10d7b02
leonsabr <leonid.a.rudenko@gmail.com>
2017-03-29 18:35:56 +0300

    fix mvn test for spek

--- src/main/java/Printer.java
+++ src/main/java/Printer.java
@@ -2,9 +2,7 @@
  * Created by nikita on 01.02.17.
  */
 public class Printer {
-
-
-    public static String sayHello(String word){
-        return "Hello "+word;
+    public static String sayHello(String word) {
+        return "Hello, " + word;
     }
 }
--- src/test/kotlin/PrinterSpek.kt
+++ src/test/kotlin/PrinterSpek.kt
@@ -1,21 +1,21 @@
+import Printer.sayHello
 import org.jetbrains.spek.api.Spek
 import org.jetbrains.spek.api.dsl.given
 import org.jetbrains.spek.api.dsl.it
 import org.jetbrains.spek.api.dsl.on
+import org.junit.jupiter.api.Assertions.assertEquals
 
 /**
  * Created by nikita on 01.02.17.
  */
 class PrinterSpek : Spek({
-    val v = "World"
-    given("A printer "){
-        on("Word "+v){
-            val s = "'Hello, World'"
-            it("Should return $s"){
-                //TODO assert
+    val word = "World"
+    given("A printer") {
+        on("Word $word") {
+            val expected = "Hello, $word"
+            it("should return '$expected'") {
+                assertEquals(expected, sayHello(word))
             }
         }
-
     }
-
 })
